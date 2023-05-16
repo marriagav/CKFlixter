cb547a667923dfb3b5a65d24f52d427215420788
Miguel Arriaga Velasco <marriagav3@gmail.com>
2023-05-16 13:20:10 -0600

    Initial Commit

---
+++ Flixter/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Flixter
+//
+//  Created by Miguel Arriaga Velasco on 16/05/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Flixter/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Flixter
+//
+//  Created by Miguel Arriaga Velasco on 16/05/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
